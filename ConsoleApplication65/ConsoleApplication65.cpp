888f76a822e31ec551395a4bcba91e035048172b
Александр Александр <stripalol@inbox.ru>
2024-04-02 15:33:04 +0700

    сделал

--- ConsoleApplication65/ConsoleApplication65.cpp
+++ ConsoleApplication65/ConsoleApplication65.cpp
@@ -3,8 +3,11 @@
 #include <set>
 #include <list>
 #include <map>
-
+#include <hash_map>
+#include <hash_set>
 using namespace std;
+#pragma region MyRegion
+
 //поиск дубликатов
 //функция принимает массив, а возвращает bool
 
@@ -44,8 +47,6 @@ vector<int> duplicateValues(const vector<int>& arr)
 	}
 	return result;
 }
-
-
 // O(N) - временная, O(N/2) - по памяти 
 map<int, int> dublicatesValues2(const vector<int>& arr)
 {
@@ -57,17 +58,40 @@ map<int, int> dublicatesValues2(const vector<int>& arr)
 	}
 	return result;
 }
+#pragma endregion
+
+
+
 
 int main()
 {
+#pragma region MyRegion
+
 	vector<int> test{ 1, 5, 1, 3, 2, 3, 8 };
-	/*for (auto item : duplicateValues(test))
+	
+	for (auto item : duplicateValues(test))
 	{
 		cout << item << endl;
-	}*/
-
+	}
+	
 	for (auto i : dublicatesValues2(test))
 	{
 		cout << i.first << " : " << i.second << endl;
 	}
+#pragma endregion
+
+	hash_set<int> hset1;
+	hset1.bucket(50);
+	hset1.insert(5);
+	/* 
+		если есть порядок данных list<>, forward_list<>, vector<>
+		если известно количесво данных vector<>
+		если порядок добавления не важен set<>, map<>, hash_set<>, hash_map<>
+		если необходимо добавлять значение в конец list<>, или forward_list<>
+		если непонятно какую структуру взять то лучше выбрать set<> или map<>
+		^^^
+		|||
+		если нужно хранить небольшое кол-во значений hash_set<>, hash_map<>
+	*/
+
 }
